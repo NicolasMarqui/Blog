c2cbd19777d942cb1acb058ea8a9fec2dc11668a
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2019-08-19 00:10:22 -0300

    Posts and side Menu done --Static--

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+#List of Disciplines
+
+disciplinas = [ 'Sistema Operacional I' , 'IHC' , 'Metodologia' , ' Estrutura de Dados '
+, 'Inglês IV', 'Programação WEB', 'Gestão de Projetos']
+
 def home(request):
     return render(
         request,
         'index.html',
         {
-            'range': range(10)
+            'range': range(10),
+            'disciplinas': disciplinas,
         }
     )
