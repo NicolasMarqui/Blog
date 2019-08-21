f1f4d3105e33132e741a528c57e46527a974fc60
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2019-08-21 00:05:19 -0300

    Database connected

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    id_post = models.AutoField(primary_key=True)
+    autor = models.CharField(max_length = 20)
+    titulo = models.CharField(max_length=255)
+    data = models.DateTimeField('Post Date')
+    semestre = models.IntegerField('Semestre')
+    discplina = models.CharField(max_length=50)
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.titulo
